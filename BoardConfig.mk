a4198fea0b22ee31b4372d36cc449aab65f330cd
M1cha <sigmaepsilon92@gmail.com>
2014-06-01 12:13:13 +0200

    use cyanogenmod_condor_defconfig as kernel config
    Change-Id: Ic2b7abe01c462b39e15edc33dd67c0cd575f05d6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,6 +28,9 @@
 
 LOCAL_PATH := device/motorola/condor
 
+# Kernel
+TARGET_KERNEL_CONFIG := cyanogenmod_condor_defconfig
+
 # Vendor Init
 TARGET_UNIFIED_DEVICE := true
 TARGET_INIT_VENDOR_LIB := libinit_msm
